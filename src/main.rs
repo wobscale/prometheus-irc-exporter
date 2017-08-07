9734d39ea35f0ef268c5818f2336954f21e55b4f
Euan Kemp <euank@euank.com>
2017-08-06 18:33:49 -0700

    Update to use 'target' query parameter
    It still returns a dummy 'up' response, but this is still a step in the
    right direction

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_derive;
 extern crate toml;
 #[macro_use]
 extern crate clap;
+extern crate url;
 
 use std::fs::File;
 use std::io::{Read, Cursor};
@@ -13,66 +14,50 @@ use clap::{App, Arg};
 use prometheus::{Opts, Gauge, Encoder};
 use rocket::State;
 use rocket::handler::Outcome;
-use rocket::http::Method;
+use rocket::http::{Status, Method};
 use rocket::{Request, Route};
 
-#[derive(Deserialize)]
-struct Server {
-    name: String,
-    host: String,
-    ports: Option<Vec<u16>>,
-    tls_ports: Option<Vec<u16>>,
-}
-
-#[derive(Deserialize)]
-struct Config {
-    servers: Vec<Server>,
-}
-
 fn main() {
-    let app = App::new(crate_name!())
-        .author(crate_authors!())
-        .version(crate_version!())
-        .arg(Arg::with_name("config")
-            .help("config file path")
-            .takes_value(true)
-            .short("c")
-            .required(true)
-            .long("config"))
-        .get_matches();
-
-    let config = app.value_of("config").unwrap();
-
-    let file = File::open(config).expect("could not open config file");
-
-    let contents =
-        file.bytes().collect::<Result<Vec<u8>, std::io::Error>>().expect("could not read file");
-
-    let cfg = match toml::from_str::<Config>(&String::from_utf8(contents).unwrap()) {
-        Ok(c) => c,
-        Err(e) => panic!("could not parse toml: {}", e),
-    };
-
     let metrics_route = Route::new(Method::Get, "/metrics", metrics);
 
-    rocket::ignite()
-        .manage(cfg)
-        .mount("/", vec![metrics_route])
-        .launch();
+    rocket::ignite().mount("/", vec![metrics_route]).launch();
 }
 
 fn metrics<'r>(request: &'r Request, _: rocket::Data) -> rocket::handler::Outcome<'r> {
-    let cfg = request.guard::<State<Config>>().unwrap().inner();
+    let query = request.uri().query().unwrap();
+    let target = rocket::request::FormItems::from(query)
+        .find(|&el| el.0 == "target")
+        .map(|s| s.1.url_decode());
+
+    let target = match target {
+        Some(t) => t,
+        None => {
+            return Outcome::failure(Status::new(400, "no target"));
+        }
+    };
+
+    let target = match target {
+        Ok(t) => t,
+        Err(_) => {
+            return Outcome::failure(Status::new(400, "invalid target"));
+        }
+    };
+
+    let irc_url = match url::Url::parse(&target) {
+        Ok(i) => i,
+        Err(e) => {
+            return Outcome::failure(Status::new(400, "target must be a valid irc uri"));
+        }
+    };
+
+    if irc_url.scheme() != "irc" && irc_url.scheme() != "ircs" {
+        return Outcome::failure(Status::new(400, "target must be a valid irc uri"));
+    }
 
     let r = prometheus::Registry::new();
 
-    for server in &cfg.servers {
-        let up = Gauge::with_opts(Opts::new(format!("{}_up", server.name),
-                                            "server is up".to_string()))
-            .unwrap();
-        // TODO: add actual metrics here
-        up.inc();
-        r.register(Box::new(up)).unwrap();
+    for metrics in scrape_irc_server(irc_url) {
+        r.register(metrics);
     }
 
     let encoder = prometheus::TextEncoder::new();
@@ -82,4 +67,18 @@ fn metrics<'r>(request: &'r Request, _: rocket::Data) -> rocket::handler::Outcom
     resp.streamed_body(Cursor::new(metric_body));
 
     Outcome::from(request, resp.finalize())
+}
+
+fn scrape_irc_server(target: url::Url) -> Vec<Box<prometheus::Collector>> {
+    let up = Gauge::with_opts(Opts::new("up", "server is up")).unwrap();
+    let tls_expiration = Gauge::with_opts(Opts::new("tls_expiration", "server tls expiration"))
+        .unwrap();
+    let global_users = Gauge::with_opts(Opts::new("global_users", "number of global users"))
+        .unwrap();
+    let local_users = Gauge::with_opts(Opts::new("local_users", "number of local users")).unwrap();
+    let uptime = Gauge::with_opts(Opts::new("uptime", "server uptime")).unwrap();
+
+
+    up.inc();
+    vec![Box::new(up)]
 }
